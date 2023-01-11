ce178c1862f2deefe7601be0f9b6623299736819
balikdavid <david.balik@tul.cz>
2023-01-11 13:34:12 +0100

    v. pong table creeated without barriers

---
+++ js/materials.js
@@ -0,0 +1,13 @@
+
+ const materials = {
+    instrument1: new THREE.MeshLambertMaterial({color: 0x6F1AB6}),//bemic 000080
+    instrument2: new THREE.MeshLambertMaterial({color: 0x6F1AB6}),
+    plane: new THREE.MeshLambertMaterial({color: 0x1919}),
+    table: new THREE.MeshLambertMaterial({color: 0x15443f}),
+    barrier: new THREE.MeshLambertMaterial({color: 0x261260}),
+    ball: new THREE.MeshLambertMaterial({color: 0x6F1AB6}), //0x4d4d4d bemic 810CA8-ja
+};
+
+export default materials;
+
+ 
